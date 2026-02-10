3f7dd56d0e2cdf4d5e0cfd27056c7644385a3e6b
tomacruzjosh <tomacruzj415@gmail.com>
2026-02-10 01:06:15 +0000

    Fixed some bugs with AccountTest and ClosedState.

--- ClosedState.java
+++ ClosedState.java
@@ -3,19 +3,17 @@ public class ClosedState implements AccountState {
     System.out.println("You cannot deposit on a closed account!");
     System.out.println(account.toString());
   }
-  public void withdraw (Account account, Double withdrawAmount) {
+  public void withdraw(Account account, Double withdrawAmount) {
     System.out.println("You cannot withdraw on a closed account!");
     System.out.println(account.toString());
   }
-  public void suspend (Account account) {
+  public void suspend(Account account) {
     System.out.println("You cannot suspend a closed account!");
   }
   public void activate(Account account) {
-    account.setAccountState(new ActiveState());
     System.out.println("You cannot activate a closed account!");
   }
   public void close(Account account) {
-    account.setAccountState(new ClosedState());
     System.out.println("Account is already closed!");
   }
 }
