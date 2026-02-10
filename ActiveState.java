daf7f2ca8dd1deb3db6e169390b0bf58074805f8
tomacruzjosh <tomacruzj415@gmail.com>
2026-02-10 08:39:07 +0800

    Update ActiveState.java

--- ActiveState.java
+++ ActiveState.java
@@ -1,3 +1,21 @@
 public class ActiveState implements AccountState {
-  
+  public void deposit(Account account, Double depositAmount) {
+    account.setBalance(account.getBalance() + depositAmount);
+    System.out.println(account.toString());
+  }
+  public void withdraw (Account account, Double withdrawAmount) {
+    account.setBalance(account.getBalance() - withdrawAmount);
+    System.out.println(account.toString());
+  }
+  public void suspend (Account account) {
+    account.setAccountState(new SuspendedState());
+    System.out.println("Account is suspended!");
+  }
+  public void activate(Account account) {
+    System.out.println("Account is already activated!");
+  }
+  public void close(Account account) {
+    account.setAccountState(new ClosedState());
+    System.out.println("Account is closed!");
+  }
 }
